512a4faae9f54b30c350b938f265850164519c55
jdberg <jb99459@ravens.racc.edu>
2018-02-04 02:07:41 -0500

    added assignment-03 file and changed test.js file

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
-alert("Hello, World!");
+Var youLikeMeat = true;
+var myNum = 10;
+
+if (youLikeMeat) {
+
+    document.write ("you like meat");
+
+} else {
+
+    document.write("you hate meat");
+
+}
